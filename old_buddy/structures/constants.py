5e0696c386e0e6faa01b89b6eb7714503fe544cb
Tomáš Jozífek <tomas.jozifek@prusa3d.cz>
2020-09-16 14:19:05 +0200

    Improve FINISHED reporting

--- old_buddy/structures/constants.py
+++ old_buddy/structures/constants.py
@@ -2,4 +2,6 @@ from old_buddy.structures.model_classes import States
 
 BASE_STATES = {States.READY, States.BUSY}
 PRINTING_STATES = {States.PRINTING, States.PAUSED, States.FINISHED}
+
+JOB_ONGOING_STATES = {States.PRINTING, States.PAUSED}
 JOB_ENDING_STATES = BASE_STATES.union({States.FINISHED, States.ERROR})
