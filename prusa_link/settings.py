ad3d28aae6bf0a183a62742d94eca24707304407
Tomáš Jozífek <tomas.jozifek@prusa3d.cz>
2020-10-01 19:45:08 +0200

    Don't save defaults into user settings, save them into an example

--- prusa_link/settings.py
+++ prusa_link/settings.py
@@ -20,12 +20,30 @@ class Settings:
     FOO = SETTINGS.FOO
     """
 
+    # Changed before 0.0.3 - defaults aren't saved in the config file
+    # Now we only create an example config file and the user is expected to
+    # copy what he wishes to change
+    # This way, we can update the default values and not get overriden by
+    # previous ones already in the config
+
+    @staticmethod
+    def create_example_settings(path, settings_data_class):
+        directory = os.path.dirname(path)
+        example_path = os.path.join(directory, "config_example.yaml")
+
+        example_dict = settings_data_class().dict()
+        example_yaml = dump(example_dict, Dumper=Dumper)
+        with open(example_path, 'w') as example_file:
+            example_file.write(example_yaml)
+
     def __init__(self, settings_data_class: Type[BaseModel], path):
         self.path = path
 
         if not os.path.exists(self.path):
             os.makedirs(os.path.dirname(self.path), exist_ok=True)
             open(self.path, 'a').close()
+
+        Settings.create_example_settings(path, settings_data_class)
 
         with open(self.path, "r") as settings_file:
             settings_yaml = settings_file.read()
@@ -38,11 +56,3 @@ class Settings:
             log.debug(f"Read dict {settings_dict}")
             self.settings = settings_data_class(**settings_dict)
             log.debug(f"Settings dict {self.settings.dict()}")
-        self.save()
-
-    def save(self):
-        settings_dict = self.settings.dict()
-        settings_yaml = dump(settings_dict, Dumper=Dumper)
-
-        with open(self.path, 'w') as settings_file:
-            settings_file.write(settings_yaml)
