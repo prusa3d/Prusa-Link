29370224ee993b352e99bb440626e6c08b1f614f
Tomáš Jozífek <tomas.jozifek@prusa3d.cz>
2020-10-11 20:40:02 +0200

    Fix setup trying to install wiringpi on non Pi systems

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
+import os
+
 from setuptools import setup, find_packages
 
 from prusa_link import __version__, __doc__
 
+RPI_MODEL_PATH = "/sys/firmware/devicetree/base/model"
+
+is_raspberry = False
+
+try:
+    if os.path.exists(RPI_MODEL_PATH):
+        with open(RPI_MODEL_PATH) as model_file:
+            model = model_file.read()
+            if "Pi" in model:
+                is_raspberry = True
+except:
+    print("This is not a Raspberry Pi -> wiringpi installation won't be "
+          "attempted!")
+
+if is_raspberry:
+    requirements_path = "requirements-pi.txt"
+else:
+    requirements_path = "requirements.txt"
+
 
 REQUIRES = []
-with open("requirements.txt", "r") as requires:
-    for line in requires:
+with open(requirements_path, "r") as requirements_path:
+    for line in requirements_path:
         REQUIRES.append(line.strip())
 
 
