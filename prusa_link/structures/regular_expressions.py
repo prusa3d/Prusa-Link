219aae295a784ccc68b2e356cf70b43732499c20
Tomáš Jozífek <tomas.jozifek@prusa3d.cz>
2020-10-16 21:04:29 +0200

    Report percentages for flow and extrusion multipliers

--- prusa_link/structures/regular_expressions.py
+++ prusa_link/structures/regular_expressions.py
@@ -6,6 +6,7 @@ OPEN_RESULT_REGEX = re.compile(r"^(File opened).*|^(open failed).*")
 PRINTER_TYPE_REGEX = re.compile(r"^(\d{3,5})$")
 FW_REGEX = re.compile(r"^FIRMWARE_NAME:Prusa-Firmware ?((\d+\.)*\d).*$")
 NOZZLE_REGEX = re.compile(r"^(\d\.\d+)$")
+PERCENT_REGEX = re.compile(r"^(\d{0,3})%$")
 
 
 BEGIN_FILES_REGEX = re.compile(r"^Begin file list$")
