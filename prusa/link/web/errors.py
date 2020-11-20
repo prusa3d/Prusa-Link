b3be539921ed025dd2242721ddf53bbb9e7a87d3
Ondrej Tuma <mcbig@zeropage.cz>
2020-11-20 13:27:10 +0100

    info page

---
+++ prusa/link/web/errors.py
@@ -0,0 +1,44 @@
+"""Zakladní obecná obsluha url."""
+from sys import exc_info
+
+from traceback import format_tb, format_exc
+
+from poorwsgi.response import make_response
+
+from ..config import log_http as log
+from .lib.view import generate_page
+from .lib.core import app
+
+
+@app.http_state(500)
+def internal_server_error(req):
+    """Obsluha chyby 500 Internal Server Error."""
+    type_, error, traceback = exc_info()    # pylint: disable=unused-variable
+    traceback = format_tb(traceback)
+    log.error('\n%s', ''.join(traceback))
+    try:
+        kwargs = {}
+        if app.debug:
+            kwargs["traceback"] = traceback
+
+        return make_response(generate_page(req, "error500.html",
+                                           error=repr(error), **kwargs),
+                             status_code=500)
+    except Exception:
+        traceback = format_exc()
+        log.error(traceback)
+        return "500 - Service Unavailable", 500
+
+
+@app.http_state(403)
+def forbidden(req):
+    """obsluha chyby 403 forbidden."""
+    return make_response(generate_page(req, "error403.html", error=exc_info()),
+                         status_code=403)
+
+
+@app.http_state(404)
+def http_state(req):
+    """Obsluha chyby 404 Not Found."""
+    return make_response(generate_page(req, "error404.html", error=exc_info()),
+                         status_code=404)
