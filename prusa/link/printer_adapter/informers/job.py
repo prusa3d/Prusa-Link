5020d3b3252e62da4f3f3512a63639469f653a78
Tomáš Jozífek <tomas.jozifek@prusa3d.cz>
2020-11-21 23:05:44 +0100

    Implement JOB_INFO

--- prusa/link/printer_adapter/informers/job.py
+++ prusa/link/printer_adapter/informers/job.py
@@ -33,34 +33,45 @@ class Job:
         self.job_path = get_clean_path(PATH.JOB_FILE)
         ensure_directory(os.path.dirname(self.job_path))
 
+        self.job_start_cmd_id = None
+        self.printing_file_path = None
+
         if os.path.exists(self.job_path):
             with open(self.job_path, "r") as job_file:
-                data_parts = job_file.read().split(" ")
+                data_parts = job_file.read().split(" ", 3)
                 self.job_id = int(data_parts[0])
                 self.job_state = JobState(data_parts[1])
+                if len(data_parts) == 4:
+                    if data_parts[2] != "":
+                        self.job_start_cmd_id = int(data_parts[2])
+                    if data_parts[3] != "":
+                        self.printing_file_path = data_parts[3]
         else:
             self.job_id = 0
             self.job_state = JobState.IDLE
 
         self.job_id_updated_signal.send(self, job_id=self.get_job_id())
 
-    def job_started(self):
+    def job_started(self, command_id=None):
         self.job_id += 1
+        self.job_start_cmd_id = command_id
         self.change_state(JobState.IN_PROGRESS)
         self.write()
         log.debug(f"New job started, id = {self.job_id}")
         self.job_id_updated_signal.send(self, job_id=self.get_job_id())
 
     def job_ended(self):
+        self.job_start_cmd_id = None
+        self.printing_file_path = None
         self.change_state(JobState.IDLE)
         log.debug(f"Job ended")
         self.job_id_updated_signal.send(self, job_id=self.get_job_id())
 
-    def state_changed(self, from_state, to_state):
+    def state_changed(self, from_state, to_state, command_id=None):
         """Called before anything regarding state is sent"""
         if from_state in BASE_STATES and to_state in PRINTING_STATES \
                 and self.job_state == JobState.IDLE:
-            self.job_started()
+            self.job_started(command_id)
         if from_state in JOB_ONGOING_STATES and to_state in JOB_ENDING_STATES \
                 and self.job_state == JobState.IN_PROGRESS:
             self.change_state(JobState.ENDING)
@@ -76,8 +87,19 @@ class Job:
         self.write()
 
     def write(self):
+        job_start_cmd_id = ""
+        if self.job_start_cmd_id is not None:
+            job_start_cmd_id = f" {self.job_start_cmd_id}"
+
+        printing_file_path = ""
+        if self.printing_file_path is not None:
+            printing_file_path = f" {self.printing_file_path}"
+
         with open(self.job_path, "w") as job_file:
-            job_file.write(f"{self.job_id} {self.job_state.value}")
+            job_file.write(" ".join([str(self.job_id),
+                                     self.job_state.value,
+                                     job_start_cmd_id,
+                                     printing_file_path]))
             job_file.flush()
             os.fsync(job_file.fileno())
 
@@ -86,3 +108,15 @@ class Job:
         log.debug(f"job_id requested, we are {self.job_state.name}")
         if self.job_state != JobState.IDLE:
             return self.job_id
+
+    def get_state(self):
+        return self.job_state
+
+    def get_file_path(self):
+        return self.printing_file_path
+
+    def get_start_cmd_id(self):
+        return self.job_start_cmd_id
+
+    def set_file_path(self, path):
+        self.printing_file_path = path
