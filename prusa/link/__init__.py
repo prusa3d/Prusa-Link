4f523143bbd4cf02b89e833e67ec54bbf11d6b3f
Ondrej Tuma <mcbig@zeropage.cz>
2020-12-01 16:52:39 +0100

    new testing release

--- prusa/link/__init__.py
+++ prusa/link/__init__.py
@@ -2,8 +2,8 @@
 __application__ = "Prusa-Link"
 __vendor__ = "PrusaResearch"
 
-__version__ = "0.1.3-dev"
-__date__ = "23 Nov 2020"
+__version__ = "0.1.3"
+__date__ = "1 Dec 2020"
 __copyright__ = "(c) 2020 Prusa 3D"
 __author_name__ = "Tomáš Jozífek"
 __author_email__ = "tomas.jozifek@prusa3d.cz"
