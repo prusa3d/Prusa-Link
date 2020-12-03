6474f64f11852bd195262e187cf90c7231df3bbe
Martin Užák <martin.uzak@gmail.com>
2020-12-03 09:04:17 +0100

    Use names required by Connect.

--- prusa/link/printer_adapter/command_handlers/job_info.py
+++ prusa/link/printer_adapter/command_handlers/job_info.py
@@ -20,11 +20,11 @@ class JobInfo(ResponseCommand):
             self.failed("Cannot get job info, "
                         "when there is no job in progress.")
 
-        values = self.state_manager.job.get_job_info_data()
+        data = self.state_manager.job.get_job_info_data()
 
         self.printer.event_cb(event=Event.JOB_INFO,
                               source=Source.CONNECT,
                               command_id=self.caller.command_id,
                               job_id=self.state_manager.job.get_job_id(),
                               state=self.state_manager.get_state().value,
-                              values=values)
+                              **data)
