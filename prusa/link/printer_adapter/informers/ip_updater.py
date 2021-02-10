df7af563179d8d3b41abf39dfc52106cdad4944b
Tomáš Jozífek <tomas.jozifek@prusa3d.cz>
2021-02-10 18:48:58 +0100

    Support M552 P<IP> to show ip in the printer support menu
    Fix CONNECT-379

--- prusa/link/printer_adapter/informers/ip_updater.py
+++ prusa/link/printer_adapter/informers/ip_updater.py
@@ -4,9 +4,13 @@ from time import time
 
 from blinker import Signal
 
+from prusa.link.printer_adapter.input_output.serial.helpers import \
+    enqueue_instruction, wait_for_instruction
+from prusa.link.printer_adapter.input_output.serial.serial_queue import \
+    SerialQueue
 from prusa.link.printer_adapter.model import Model
 from prusa.link.printer_adapter.const import IP_UPDATE_INTERVAL, \
-    SHOW_IP_INTERVAL, NO_IP
+    SHOW_IP_INTERVAL, NO_IP, IP_WRITE_TIMEOUT
 from prusa.link.printer_adapter.updatable import ThreadedUpdatable
 from prusa.link.printer_adapter.util import get_local_ip
 from prusa.link import errors
@@ -20,7 +24,8 @@ class IPUpdater(ThreadedUpdatable):
     thread_name = "ip_updater"
     update_interval = IP_UPDATE_INTERVAL
 
-    def __init__(self, model: Model):
+    def __init__(self, model: Model, serial_queue: SerialQueue):
+        self.serial_queue = serial_queue
         self.data = model.ip_updater
 
         self.updated_signal = Signal()  # kwargs: old_ip: str, new_ip: str
@@ -36,7 +41,6 @@ class IPUpdater(ThreadedUpdatable):
             errors.LAN.ok = True
         except socket.error:
             log.error("Failed getting the local IP, are we connected to LAN?")
-            self.data.local_ip = NO_IP
             self.update_ip(NO_IP)
             errors.PHY.ok = False
         else:
@@ -45,7 +49,6 @@ class IPUpdater(ThreadedUpdatable):
             if self.data.local_ip != local_ip:
                 log.debug(f"The IP has changed, or we reconnected."
                           f"The new one is {local_ip}")
-                self.data.local_ip = local_ip
                 self.update_ip(local_ip)
             elif time() > self.data.update_ip_on:
                 self.data.update_ip_on = time() + SHOW_IP_INTERVAL
@@ -54,4 +57,22 @@ class IPUpdater(ThreadedUpdatable):
     def update_ip(self, new_ip):
         old_ip = self.data.local_ip
         self.data.local_ip = new_ip
+        log.debug(f"old {old_ip} != new {new_ip} = {old_ip != new_ip}")
+        if old_ip != new_ip:
+            self.send_ip_to_printer(new_ip)
         self.updated_signal.send(self, old_ip=old_ip, new_ip=new_ip)
+
+    def send_ip_to_printer(self, ip):
+        timeout_at = time() + IP_WRITE_TIMEOUT
+        if ip == NO_IP:
+            instruction = enqueue_instruction(self.serial_queue,
+                                              "M552 P0.0.0.0")
+        else:
+            instruction = enqueue_instruction(self.serial_queue,
+                                              f"M552 P{ip}")
+        wait_for_instruction(
+            instruction, lambda: self.running and time() < timeout_at)
+
+    def stop(self):
+        self.send_ip_to_printer(NO_IP)
+        super().stop()
