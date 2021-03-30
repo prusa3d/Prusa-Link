8c857eed7649c8bdebed409707dffcb5c688d417
Ondrej Tuma <mcbig@zeropage.cz>
2021-03-30 15:55:36 +0200

    release

--- prusa/link/__init__.py
+++ prusa/link/__init__.py
@@ -2,13 +2,13 @@
 __application__ = "Prusa-Link"
 __vendor__ = "PrusaResearch"
 
-__version__ = "0.3.0.dev3"
-__date__ = "14 Dec 2020"
+__version__ = "0.3.0"
+__date__ = "30 Mar 2021"
 __copyright__ = "(c) 2020 Prusa 3D"
 __author_name__ = "Tomáš Jozífek"
 __author_email__ = "tomas.jozifek@prusa3d.cz"
 __author__ = f"{__author_name__} <{__author_email__}>"
 __description__ = f"{__application__} software"
 
-__credits__ = "Tomáš Jozífek, Ondřej Tůma"
+__credits__ = "Tomáš Jozífek, Ondřej Tůma, Michal Zoubek"
 __url__ = "https://github.com/prusa3d/Prusa-Link"
