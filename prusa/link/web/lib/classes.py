dc048b3f30478e9f871e00792aa3a0f1a17c8972
Ondrej Tuma <mcbig@zeropage.cz>
2021-04-26 15:01:54 +0200

    Use daemon type WSGI threads

--- prusa/link/web/lib/classes.py
+++ prusa/link/web/lib/classes.py
@@ -4,24 +4,17 @@ Main server classes for handling request.
 """
 import logging
 from wsgiref.simple_server import WSGIServer
-from socketserver import ForkingMixIn, ThreadingMixIn
+from socketserver import ThreadingMixIn
 
 log = logging.getLogger(__name__)
 
 
 class SingleServer(WSGIServer):
     """WSGIServer with handler error."""
-    type = "Single"
-
     def handle_error(self, request, client_address):
         log.exception("Error for client %s", client_address[0])
 
 
-class ForkingServer(ForkingMixIn, SingleServer):
-    """WSGIServer which run request in fork."""
-    type = "Forking"
-
-
 class ThreadingServer(ThreadingMixIn, SingleServer):
     """WSGIServer which run request in thread."""
-    type = "Threading"
+    daemon_threads = True
