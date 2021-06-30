0f14109fb105b821761fca129006ca32dc93100b
Ondrej Tuma <mcbig@zeropage.cz>
2021-06-30 14:09:46 +0200

    travis fix and some refactoring

--- prusa/link/web/lib/wizard.py
+++ prusa/link/web/lib/wizard.py
@@ -115,7 +115,8 @@ class Wizard:
         url = Printer.connect_url(self.connect_hostname,
                                   bool(self.connect_tls), self.connect_port)
         try:
-            urlopen(f'{url}/info')
+            with urlopen(f'{url}/info'):
+                pass
         except Exception:  # pylint: disable=broad-except
             errors['connection'] = True
         self.errors['connect'] = errors
