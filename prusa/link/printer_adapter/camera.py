c7219d3014a78b307151f72950a8edaf10515e06
Jaroslav Malec <dzardacz@gmail.com>
2021-08-03 20:38:18 +0100

    Add basic camera class

---
+++ prusa/link/printer_adapter/camera.py
@@ -0,0 +1,24 @@
+import sys
+import io
+import picamera
+
+class Camera:
+    """Provides access to the printer camera."""
+    def __init__(self):
+        self.camera = None
+
+    def setup(self, parameters):
+        """Deferred initialization to allow multiple setup calls"""
+        """on the same instance with varying parameters."""
+        if self.camera is None:
+            self.camera = picamera.PiCamera()
+
+    def capture(self, stream):
+        """Captures a camera frame and saves it into stream."""
+        self.camera.capture(stream, 'jpeg')
+
+
+if __name__ == '__main__':
+    cam = Camera()
+    cam.setup({})
+    cam.capture(open('image.jpg', 'wb'))
