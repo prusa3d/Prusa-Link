ca25cb2c6032a7b27319e1b98ac57a49f782dc80
Jaroslav Malec <dzardacz@gmail.com>
2021-08-05 20:35:54 +0100

    Import picamera only when used

--- prusa/link/printer_adapter/camera.py
+++ prusa/link/printer_adapter/camera.py
@@ -1,17 +1,19 @@
 import sys
 import io
-import picamera
+import importlib
 
 class Camera:
     """Provides access to the printer camera."""
     def __init__(self):
+        """Import picamera programmatically to avoid errors when not on RPi (CI)"""
+        self.picamera_module = importlib.import_module('picamera')
         self.camera = None
 
     def setup(self, parameters):
         """Deferred initialization to allow multiple setup calls"""
         """on the same instance with varying parameters."""
         if self.camera is None:
-            self.camera = picamera.PiCamera()
+            self.camera = self.picamera_module.PiCamera()
 
     def capture(self, stream):
         """Captures a camera frame and saves it into stream."""
