955c9a77f3cd19294e501dbdd097d249f8439782
Ondrej Tuma <mcbig@zeropage.cz>
2021-10-22 13:21:39 +0200

    Simple html errors
    * upload file size check fix

--- prusa/link/web/errors.py
+++ prusa/link/web/errors.py
@@ -2,96 +2,97 @@
 import logging
 from sys import exc_info
 
-from traceback import format_tb, format_exc
+from traceback import format_tb
 
-from poorwsgi.response import make_response, JSONResponse, TextResponse
+from poorwsgi.response import make_response
 
 from .lib.view import generate_page
 from .lib.core import app
-from ..errors import LinkError
+from .. import errors
 
 log = logging.getLogger(__name__)
 
 
+def response_error(req, error: errors.LinkError):
+    """Create response from LinkError"""
+    error.set_url(req)
+    if req.accept_json:
+        return error.json_response()
+    if req.accept_html:
+        return make_response(generate_page(req, error.template),
+                             status_code=error.status_code)
+    return error.text_response()
+
+
 @app.http_state(500)
+@app.route('/error/internal-server-error')
 def internal_server_error(req):
     """Error handler 500 Internal Server Error."""
-    type_, error, traceback = exc_info()  # pylint: disable=unused-variable
-    traceback = format_tb(traceback)
-    log.error('\n%s%s', ''.join(traceback), repr(error))
-    try:
-        kwargs = {}
-        if app.debug:
-            kwargs["traceback"] = traceback
+    type_, exception, traceback = exc_info()  # pylint: disable=unused-variable
+    if req.path != '/error/internal-server-error':
+        traceback = format_tb(traceback)
+        log.error('\n%s%s', ''.join(traceback), repr(exception))
 
-        return make_response(generate_page(req,
-                                           "error500.html",
-                                           error=repr(error),
-                                           **kwargs),
-                             status_code=500)
+    error = errors.InternalServerError()
+    error.set_url(req)
+
+    try:
+        if req.accept_json:
+            return error.json_response()
+        if req.accept_html:
+            kwargs = {}
+            if app.debug and traceback:
+                kwargs["traceback"] = traceback
+
+            return make_response(generate_page(req,
+                                               error.template,
+                                               error=repr(exception),
+                                               **kwargs),
+                                 status_code=500)
+
     except Exception:  # pylint: disable=broad-except
-        traceback = format_exc()
-        log.error(traceback)
-        return "500 - Service Unavailable", 500
+        log.exception()
+    return error.text_response()
 
 
 @app.http_state(403)
+@app.route('/error/forbidden')
 def forbidden(req):
     """Error handler 403 forbidden."""
-    return make_response(generate_page(req, "error403.html", error=exc_info()),
-                         status_code=403)
+    print(">>> Forbidden voe")
+    return response_error(req, errors.ForbiddenError())
 
 
 @app.http_state(404)
+@app.route('/error/not-found')
 def not_found(req):
     """Error handler for 404 Not Found."""
-    return make_response(generate_page(req, "error404.html", error=exc_info()),
-                         status_code=404)
+    return response_error(req, errors.NotFoundError())
 
 
 @app.http_state(410)
 def gone(req):
-    """Error handler for 410 Gone."""
+    """Error handler for 410 Gone.
+
+    This handler is called only when wizard is done and someone try to
+    access it.
+    """
     return make_response(generate_page(req, "error410.html", error=exc_info()),
                          status_code=410)
 
 
 @app.http_state(503)
+@app.route('/error/printer-unavailable')
 def service_unavailable(req):
     """Error handler for 503 Service Unavailable."""
     type_, error, traceback = exc_info()  # pylint: disable=unused-variable
     traceback = format_tb(traceback)
     log.error('\n%s%s', ''.join(traceback), repr(error))
 
-    if req.accept_json:
-        return JSONResponse(message="Prusa Link not finished initializing. "
-                            "Please try again later",
-                            status_code=503)
-
-    kwargs = {}
-    if app.debug:
-        kwargs["traceback"] = traceback
-
-    return make_response(generate_page(req,
-                                       "error503.html",
-                                       error=repr(error),
-                                       **kwargs),
-                         status_code=503)
+    return response_error(req, errors.PrinterUnavailable())
 
 
-@app.error_handler(LinkError)
+@app.error_handler(errors.LinkError)
 def link_error_handler(req, error):
     """Handle LinkError exception and generate right response."""
-    url = req.construct_url(error.path)
-    headers = {'Content-Location': url}
-    if req.accept_json:
-        return JSONResponse(status_code=error.status_code,
-                            headers=headers,
-                            title=error.title,
-                            text=error.text,
-                            url=url)
-
-    # pylint: disable=consider-using-f-string
-    return TextResponse("%s\n%s" % (error.title, error.text),
-                        status_code=error.status_code,
-                        headers=headers)
+    return response_error(req, error)
