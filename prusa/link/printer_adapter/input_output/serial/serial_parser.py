e890861453f5938deee3a068a638fa5d33db400a
Tomáš Jozífek <tomas.jozifek@prusa3d.cz>
2022-04-11 21:49:49 +0200

    Add multiline parsing, do cleanup and add tests for this component

--- prusa/link/printer_adapter/input_output/serial/serial_parser.py
+++ prusa/link/printer_adapter/input_output/serial/serial_parser.py
@@ -2,12 +2,25 @@
 Contains implementation of the SerialParser and Regex pairing classes
 The latter is used by the former for tracking which regular expressions
 have which handlers
+
+As of writing this doc, the "ok" has infinite priority, then every instruction
+handler has the current time as the priority, meaning later added handlers are
+evaluated first.
+
+There is a feature for multiline matches but it only works if no other handler
+matches before it. If however we're expecting another lines of the multiline
+match it takes priority over everything else until it succeeds or fails to
+match.
+
+If a multiline handler fails to match, the lines thought to be a part of the
+multiline are tried as single line.
 """
 import logging
 import re
+from functools import partial
 from re import Match
 from threading import Lock
-from typing import Dict, List, Callable
+from typing import Dict, List, Callable, Optional, Any, Union
 
 from blinker import Signal  # type: ignore
 from sortedcontainers import SortedKeyList  # type: ignore
@@ -22,11 +35,11 @@ class RegexPairing:
     An object representing a bound regexp to its handler, with priority,
     for us to be able to sort which regexps to try first
     """
-    def __init__(self, regexp, priority=0, stops_matching=True):
+    def __init__(self, regexp, priority=0, lines=1):
         self.regexp: re.Pattern = regexp
         self.signal: Signal = Signal()
         self.priority: int = priority
-        self.stops_matching = stops_matching
+        self.lines: int = lines
 
     def __str__(self):
         receiver_count = len(self.signal.receivers)
@@ -38,10 +51,26 @@ class RegexPairing:
     def __repr__(self):
         return self.__str__()
 
+    def fire(self, match=None, matches=None):
+        """
+        Fire the associated signal, catch and log errors, don't want to
+        kill the serial reading component
+        """
+        # pylint: disable=broad-except
+        try:
+            log.debug("Matched %s calling %s", self, self.signal.receivers)
+            if match is not None:
+                self.signal.send(self, match=match)
+            if matches is not None:
+                self.signal.send(self, matches=matches)
+        except Exception:
+            log.exception("Exception during handling of the printer output. "
+                          "Caught to stay alive.")
+
 
 class SerialParser(metaclass=MCSingleton):
     """
-    It's job is to try and find an appropriate handler for every line that
+    Its job is to try and find an appropriate handler for every line that
     we receive from the printer
     """
     def __init__(self):
@@ -49,34 +78,97 @@ class SerialParser(metaclass=MCSingleton):
         self.pattern_list = SortedKeyList(key=lambda item: -item.priority)
         self.pairing_dict: Dict[List[Callable[[(Match, )], None]]] = {}
 
+        self.multiline_pairing: Optional[RegexPairing] = None
+        self.remaining_lines: int = 0
+        self.multiline_matches = []
+        self.multiline_line_buffer = []
+
+        self.handler_calls = []
+
     def decide(self, line):
+        """
+        In Single line mode, it executes decide_singl_line, that can switch
+        to multiline mode.
+        In multiline mode it calls decide_multiline, depending on its result
+        continues as usual or cleans up the lines, thet were previously
+        thought to be multiline as single line
+        """
+
+        with self.lock:
+            if self.remaining_lines:
+                if not self._decide_multiline(line):
+                    for buffered_line in self.multiline_line_buffer:
+                        self._decide_single_line(
+                            buffered_line, allow_multiline=False)
+                    self.multiline_line_buffer.clear()
+
+            else:
+                self._decide_single_line(line)
+
+        for handler_call in self.handler_calls:
+            handler_call()
+        self.handler_calls.clear()
+
+    def _decide_multiline(self, line):
+        """
+        Separate multiline mode parsing method,
+        adds every line tried in multiline mode to a buffer
+        if multiline fails, this buffer will need to be tried in forced
+        single line mode
+        """
+        self.multiline_line_buffer.append(line)
+        pairing = self.multiline_pairing
+        match = pairing.regexp.match(line)
+        if not match:
+            number = pairing.lines - self.remaining_lines
+            log.debug("Multiline parsing failed at line %s/%s, parsing buffer "
+                      "as single lines",
+                      number, pairing.lines)
+            self._end_multiline()
+            return False
+
+        self.multiline_matches.append(match)
+        self.remaining_lines -= 1
+        number = pairing.lines - self.remaining_lines
+        log.debug("Multiline %s/%s", number, pairing.lines)
+        if not self.remaining_lines:
+            self.handler_calls.append(
+                partial(pairing.fire, matches=self.multiline_matches))
+            self.multiline_line_buffer.clear()
+            self._end_multiline()
+        return True
+
+    def _decide_single_line(self, line, allow_multiline=True):
         """
         The meat of the class, trying different RegexPairings ordered
         by their priorities, to find the matching one
 
+        if it finds a multiline pairing, activates multiline mode
         """
-        signal_list = []
+        for pairing in self.pattern_list:
+            match = pairing.regexp.match(line)
+            if match:
+                if allow_multiline and pairing.lines > 1:
+                    self.multiline_pairing = pairing
+                    self.remaining_lines = pairing.lines - 1
+                    self.multiline_matches = [match]
+                    self.multiline_line_buffer = [line]
+                    return
+                if pairing.lines == 1:
+                    self.handler_calls.append(
+                        partial(pairing.fire, match=match))
+                    return
+        log.debug("Match not found for")
 
-        with self.lock:
-            log.debug("Deciding on handlers for line: %s", repr(line))
-            for pairing in self.pattern_list:
-                match = pairing.regexp.match(line)
-                if match:
-                    log.debug("Successfully matched %s",
-                              pairing.regexp.pattern)
-                    signal_list.append(pairing.signal)
-                    if pairing.stops_matching:
-                        break
+    def _end_multiline(self):
+        """Cancels the multiline mode"""
+        self.multiline_pairing = None
+        self.remaining_lines = 0
+        self.multiline_matches = []
 
-            if not signal_list:
-                log.debug("Match not found")
-
-        for signal in signal_list:
-            signal: Signal
-            log.debug("calling %s", signal.receivers)
-            signal.send(self, match=match)
-
-    def add_handler(self, regexp, handler, priority=None, stops_matching=None):
+    def add_handler(self, regexp: re.Pattern,
+                    handler: Callable[[Any, re.Match], None],
+                    priority: Union[float, int] = 0):
         """
         Add an entry to output handlers.
         :param regexp: if this matches, your handler will get called
@@ -87,36 +179,51 @@ class SerialParser(metaclass=MCSingleton):
         :param priority: Higher priority means the regexp will be attempted
         sooner in the list. For items with the same priority, the newest gets
         used first
-        :param stops_matching: Workaround that shall never be used!
         """
+        self._add_handler(regexp, handler, priority, lines=1)
+
+    def add_multiline_handler(self, regexp: re.Pattern,
+                              handler: Callable[[Any, List[re.Match]], None],
+                              lines: int, priority: Union[float, int] = 0):
+        """
+        Add a multiline entry to output handlers.
+        :param regexp: if this matches, your handler will get called
+        Warning, should be unique, or the exact same as another one,
+        after the first match, the matching is stopped! and all the handlers
+        for the regexp are called
+        :param handler: Callable that will handle the list of matches
+        :param lines: number of lines to watch for
+        :param priority: Higher priority means the regexp will be attempted
+        sooner in the list. For items with the same priority, the newest gets
+        used first
+        """
+        if lines <= 1:
+            raise ValueError("A multiline handler needs more than one line")
+        self._add_handler(regexp, handler, priority, lines=lines)
+
+    def _add_handler(self, regexp, handler, priority, lines):
+        """Adds handlers for parsing regexp output, internal, doc in external
+        methods"""
         with self.lock:
             if regexp in self.pairing_dict:
                 pairing: RegexPairing = self.pairing_dict[regexp]
+                if pairing.lines != lines:
+                    raise ValueError(
+                        "Cannot register the same regular expression with "
+                        "different amounts of lines!")
                 if pairing not in self.pattern_list:
                     log.debug("%s is not in %s. What?!", pairing,
                               self.pattern_list)
-
-                if stops_matching is not None and \
-                        stops_matching != pairing.stops_matching:
-                    raise RuntimeError("Cannot add the same regexp with "
-                                       "different stops_matching parameters "
-                                       "than already known.")
-                if priority is not None:
-                    if priority > pairing.priority:
-                        self.pattern_list.remove(pairing)
-                        pairing.priority = priority
-                        self.pattern_list.add(pairing)
-                        log.debug("Priority updated from %s to %s",
-                                  pairing.priority, priority)
+                if priority > pairing.priority:
+                    self.pattern_list.remove(pairing)
+                    pairing.priority = priority
+                    self.pattern_list.add(pairing)
+                    log.debug("Priority updated from %s to %s",
+                              pairing.priority, priority)
                 pairing.signal.connect(handler, weak=False)
             else:
-                pairing_kwargs = {}
-                if priority is not None:
-                    pairing_kwargs["priority"] = priority
-                if stops_matching is not None:
-                    pairing_kwargs["stops_matching"] = stops_matching
-                pairing: RegexPairing = RegexPairing(regexp, **pairing_kwargs)
-
+                pairing: RegexPairing = RegexPairing(
+                    regexp, priority=priority, lines=lines)
                 pairing.signal.connect(handler, weak=False)
 
                 self.pairing_dict[regexp] = pairing
