26c21936798df0a2b8858ab70f2bf3b3ba7d12ce
Tomáš Jozífek <tomas.jozifek@prusa3d.cz>
2022-04-20 11:52:26 +0200

    Revert multiline support, it does not make sense to use for this

--- tests/test_serial_parser.py
+++ tests/test_serial_parser.py
@@ -2,8 +2,6 @@
 import re
 from unittest.mock import Mock
 
-import pytest
-
 from prusa.link.printer_adapter.input_output.serial.serial_parser import SerialParser  # type:ignore
 
 
@@ -92,80 +90,4 @@ def test_bump_priority():
     handler2.assert_not_called()
     assert handler3.call_args.kwargs["match"].group("a") == "Hello"
     assert handler1.call_args.kwargs["match"].group("a") == "Hello"
-    SerialParser._MCSingleton__instance = None
-
-
-def test_multiline():
-    """
-    Test the basic function of parsing multiline printer output
-    """
-    regex = re.compile(r"(?P<a>Hello)|(?P<b>World!)")
-    handler = Mock()
-    parser = SerialParser()
-    parser.add_multiline_handler(regex, handler, lines=2)
-    parser.decide("Hello")
-    parser.decide("World!")
-    handler.assert_called_once()
-    assert len(handler.call_args.kwargs["matches"]) == 2
-    assert handler.call_args.kwargs["matches"][0].group("a") == "Hello"
-    assert handler.call_args.kwargs["matches"][1].group("b") == "World!"
-    SerialParser._MCSingleton__instance = None
-
-
-def test_multiline_buffering():
-    """
-    Test that failed multiline makes all the single line calls happen
-    """
-    regex1 = re.compile(r"(?P<a>Hello)|(?P<b>World!)")
-    regex2 = re.compile(r"(?P<a>Hello)")
-    regex3 = re.compile(r"(?P<a>Universe!)")
-    handler1 = Mock()
-    handler2 = Mock()
-    handler3 = Mock()
-    parser = SerialParser()
-    parser.add_multiline_handler(regex1, handler1, lines=2, priority=1)
-    parser.add_handler(regex2, handler2)
-    parser.add_handler(regex3, handler3)
-    parser.decide("Hello")
-    parser.decide("Universe!")
-    handler1.assert_not_called()
-    handler2.assert_called_once()
-    handler3.assert_called_once()
-    assert handler2.call_args.kwargs["match"].group("a") == "Hello"
-    assert handler3.call_args.kwargs["match"].group("a") == "Universe!"
-    SerialParser._MCSingleton__instance = None
-
-
-def test_multiline_not_being_great():
-    """
-    Test as a warning of what does not work!
-    It could be done in a way where both the single line and the multiline
-    handlers get called, I just did not bother, sorry
-    """
-    regex1 = re.compile(r"(?P<a>Hello)|(?P<b>World!)")
-    regex2 = re.compile(r"(?P<a>Hello)")
-    regex3 = re.compile(r"(?P<a>Universe!)")
-    handler1 = Mock()
-    handler2 = Mock()
-    handler3 = Mock()
-    parser = SerialParser()
-    parser.add_multiline_handler(regex1, handler1, lines=2, priority=0)
-    parser.add_handler(regex2, handler2, priority=1)
-    parser.add_handler(regex3, handler3, priority=1)
-    parser.decide("Hello")
-    parser.decide("World!")
-    # One might expect the multiline to get called, but no
-
-    handler1.assert_not_called()
-    handler2.assert_called_once()
-    handler3.assert_not_called()
-    assert handler2.call_args.kwargs["match"].group("a") == "Hello"
-    SerialParser._MCSingleton__instance = None
-
-
-def test_enforced_multiline():
-    """Don't allow single line multiline handlers"""
-    parser = SerialParser()
-    with pytest.raises(ValueError):
-        parser.add_multiline_handler(re.compile(""), Mock(), lines=1)
     SerialParser._MCSingleton__instance = None
