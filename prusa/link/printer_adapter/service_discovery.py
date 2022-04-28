08d5f3fbe6f253bfc0525e4a3caf9f0d4b727138
Tomáš Jozífek <tomas.jozifek@prusa3d.cz>
2022-04-28 11:24:55 +0200

    Detect whether we're routed to port 80 and advertise accordingly

--- prusa/link/printer_adapter/service_discovery.py
+++ prusa/link/printer_adapter/service_discovery.py
@@ -5,9 +5,14 @@ As of now only DNS-SD is supported
 import logging
 import socket
 
+from time import sleep
+from urllib.error import HTTPError, URLError
+from urllib.request import Request, urlopen
+
 import zeroconf
 from zeroconf import Zeroconf, ServiceInfo, NonUniqueNameException
 
+from .const import SELF_PING_TIMEOUT, SELF_PING_RETRY_INTERVAL, instance_id
 from .interesting_logger import InterestingLogRotator
 from ..config import Config
 
@@ -29,6 +34,23 @@ class ServiceDiscovery:
         self.hostname = socket.gethostname()
         self.number = 0
 
+    @staticmethod
+    def _get_port_part(port):
+        """Return the port part of an url"""
+        return "" if int(port) == 80 else f":{port}"
+
+    def is_on_port(self, port):
+        """Check, if the same instance is presented on the specified port"""
+        port_part = self._get_port_part(port)
+        url = f"http://127.0.0.1{port_part}"
+        request = Request(url, method="HEAD")
+        try:
+            with urlopen(request, timeout=SELF_PING_TIMEOUT) as response:
+                return response.headers["Instance-ID"] == str(instance_id)
+        except (HTTPError, URLError):
+            return False
+
+
     def register(self):
         """
         Registers services provided by us to be discoverable
@@ -37,17 +59,33 @@ class ServiceDiscovery:
         one _http, because we have a web server
         and one _prusa-link because why not
         """
-        self._register_service("PrusaLink", "prusalink")
-        self._register_service("PrusaLink", "http")
+        # Wait for our own instance to be reachable on the configured port
+        # if not, just try again
+        while not self.is_on_port(self.port):
+            log.warning("Can't reach our own instance at the configured "
+                        "port: %s. If just initialising, this is normal",
+                        self.port)
+            sleep(SELF_PING_RETRY_INTERVAL)
+
+        # Try to connect using the default http port
+        register_port = self.port
+        if self.is_on_port(80):
+            # if successful, register the 80 we are being forwarded to
+            register_port = 80
+            log.debug("Reached our own instance at the port 80, "
+                      "running as root or being forwarded, awesome!")
+
+        self._register_service("PrusaLink", "prusalink", register_port)
+        self._register_service("PrusaLink", "http", register_port)
 
         # legacy slicer support
-        self._register_service("PrusaLink", "octoprint")
+        self._register_service("PrusaLink", "octoprint", register_port)
 
     def unregister(self):
         """Unregisters all services"""
         self.zeroconf.unregister_all_services()
 
-    def _register_service(self, name, service_type):
+    def _register_service(self, name, service_type, port):
         """
         Registers one service given its name and type
 
@@ -58,14 +96,15 @@ class ServiceDiscovery:
         """
         number = self.number
         while True:
-            name_to_use = f"{name} at {self.hostname}:{self.port}"
+            port_part = self._get_port_part(port)
+            name_to_use = f"{name} at {self.hostname}{port_part}"
             if number > 0:
                 name_to_use += f" ({number})"
             try:
                 info = ServiceInfo(type_=f"_{service_type}._tcp.local.",
                                    name=f"{name_to_use}._{service_type}"
                                         f"._tcp.local.",
-                                   port=self.port,
+                                   port=port,
                                    server=f"{self.hostname}.local",
                                    properties={"path": "/"})
                 self.zeroconf.register_service(info)
