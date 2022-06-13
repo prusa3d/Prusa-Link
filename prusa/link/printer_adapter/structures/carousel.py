28bf34874cff2a867182f20bb8a7d0b30383606d
Tomáš Jozífek <tomas.jozifek@prusa3d.cz>
2022-06-13 19:07:14 +0200

    Refactor the LCD printer to support events and be more readable

---
+++ prusa/link/printer_adapter/structures/carousel.py
@@ -0,0 +1,249 @@
+"""Implements the helper classes for LCD printer.
+Does not depend on the rest of the PrusaLink app"""
+import math
+from collections import deque
+from copy import copy
+from time import time
+
+from typing import List, Set, Deque
+
+
+class LCDLine:
+    """Info about the text to show and the chime to play"""
+    def __init__(self, text, delay=2.0, resets_idle=False, chime_gcode=None):
+        self.text: str = text
+        self.delay: float = delay
+        self.chime_gcode = []
+        if chime_gcode is not None:
+            self.chime_gcode = chime_gcode
+        self.resets_idle = resets_idle
+        self.ends_at = time() + self.delay
+
+    def reset_end(self):
+        """Resets the message's end time, used, so the delay is the minimum
+        time the message is shown on screen"""
+        self.ends_at = time() + self.delay
+
+
+class Screen:
+    """A Screen - like an error screen, or an easter egg scrolling screen"""
+    def __init__(self, resets_idle=True, chime_gcode=None, order=0):
+        """
+        :param resets_idle: Do the messages from this screen reset the idle
+                            timer?
+        :param chime_gcode: The gcode to play when this screen is enabled
+        :param order: This is static, but could be made dynamic.
+                      The order of screens in case there's more with the
+                      same priority. Smallest goes first
+        """
+        self.resets_idle = resets_idle
+        self.chime_gcode = []
+        if chime_gcode is not None:
+            self.chime_gcode = chime_gcode
+
+        self.conditions = {}
+        self.changed = False
+
+        self.text = ""
+        self.scroll_delay = 2.0
+        self.first_line_extra = 2.0
+        self.scroll_amount = 10
+        self.last_line_extra = 1.0
+
+        # only the things with the highest priority get displayed
+        self.priority = 0
+        # if there are more than one, they get ordered by this number
+        self.order = order
+        self.enabled = False
+        self.to_chime = False
+
+    def __str__(self):
+        return f"A Screen saying {self.text}"
+
+    def lines(self):
+        """The status display has 19 usable chars (20)
+        Iterating over this cuts the text into displayable messages (lines)
+        to output, so a scrolling or paginated appearance can be achieved"""
+        remaining_text = self.text
+        while (last_index := len(remaining_text) - 19) > 0:
+            line = LCDLine(remaining_text[:19],
+                           delay=self.scroll_delay,
+                           resets_idle=self.resets_idle)
+            if remaining_text == self.text:
+                line.delay += self.first_line_extra
+            actual_scroll_amount = min(self.scroll_amount, last_index)
+            remaining_text = remaining_text[actual_scroll_amount:]
+            yield line
+        yield LCDLine(remaining_text[:19],
+                      delay=self.scroll_delay + self.last_line_extra)
+
+
+class Carousel:
+    """Manages Screens and spurious messages
+    Ignores the timing, focuses just on what line and screen to show if asked
+    """
+
+    def __init__(self, screens: List[Screen]):
+        self.screens = set(screens)
+        self.enabled_screens: Set[Screen] = set()
+        self.active_set: Set[Screen] = set()
+        self.active_screens: List[Screen] = []
+
+        self.current_screen = None
+
+        self.to_rewind = False
+        self.messages: Deque[LCDLine] = deque()
+
+        self.line_generator = self._lines()
+
+    def _lines(self):
+        """Iterating over this goes over every enabled screen with the highest
+        priority. More screens on the same priority are supported"""
+        for self.current_screen in copy(self.active_screens):
+            for line in self.current_screen.lines():
+                if self.to_rewind:
+                    self.current_screen = None
+                    return
+
+                if self.current_screen.to_chime:
+                    self.current_screen.to_chime = False
+                    line.chime_gcode = self.current_screen.chime_gcode
+                line.resets_idle = self.current_screen.resets_idle
+                yield line
+
+    def get_next(self):
+        """Handles giving out lines to show. The spurious messages
+         have priority"""
+        if self.messages:
+            self.set_rewind()
+            return self.messages.popleft()
+        try:
+            return next(self.line_generator)
+        except (StopIteration, TypeError):
+            self._rewind()
+        try:
+            return next(self.line_generator)
+        except (StopIteration, TypeError):
+            return None  # nothing to show
+
+    def _rewind(self):
+        """Re-winds to the start. This updates what lines will get output"""
+        self.to_rewind = False
+        self.line_generator = self._lines()
+
+    def set_rewind(self):
+        """Marks the carousel for a re-wind. Next time a line will be
+        requested, the carousel will start from the first Line on the first
+        Screen"""
+        self.to_rewind = True
+
+    def add_message(self, message,
+                    delay=None,
+                    chime_gcode=None,
+                    resets_idle=None):
+        """Adds a "spurious" message to be displayed.
+        Long ones (over 19 chars) aren't supported"""
+        line = LCDLine(message)
+        if delay is not None:
+            line.delay = delay
+        if chime_gcode is not None:
+            line.chime_gcode = chime_gcode
+        if resets_idle is not None:
+            line.resets_idle = resets_idle
+        self.messages.append(line)
+
+    def verify_tracked(self, screen):
+        """If the screen isn't tracked, complains"""
+        if screen not in self.screens:
+            raise ValueError("This screen is not in the carousel")
+
+    # pylint: disable=too-many-arguments
+    def set_text(self,
+                 screen,
+                 text,
+                 scroll_delay=2.0,
+                 first_line_extra=2.0,
+                 scroll_amount=10,
+                 last_line_extra=1.0):
+        """
+        Given text and parameters, it sets up the "screen" with your text
+
+        text: Text longer than 19 character gets converted into multiple lines
+        scroll delay: each screen will wait this amount before scrolling again
+        first_line_extra: Extra seconds to wait on the first screen
+        scroll_amount: How many characters to scroll > 0
+        last_line_extra: How much longer to wait on the last screen
+
+        If the text fits on a one line, set the extra delays to 0 and use
+        just the scroll delay. Anything else is undefined
+
+        The splitting functionality is in the Screen itself
+
+        Setting text to an active screen rewinds the carousel.
+        No way to rewind just the current screen as of now
+        """
+        self.verify_tracked(screen)
+        screen.changed = True
+        screen.text = text
+        screen.scroll_delay = scroll_delay
+        screen.first_line_extra = first_line_extra
+        screen.scroll_amount = scroll_amount
+        screen.last_line_extra = last_line_extra
+
+        self._react()
+
+    def enable(self, screen: Screen):
+        """Enables a screen, if it's a one with a greater or equal priority
+        than those currently shown, it will get shown"""
+        self.verify_tracked(screen)
+        if screen in self.enabled_screens:
+            return  # Has no effect
+
+        screen.to_chime = True
+        self.enabled_screens.add(screen)
+        self._react()
+
+    def set_priority(self, screen: Screen, priority):
+        """Sets a priority to a screen, if it ends up with a higher or equal
+        one, than the ones shown, and is enabled, it will get shown"""
+        self.verify_tracked(screen)
+        if priority == screen.priority:
+            return  # has no effect
+
+        screen.priority = priority
+        self._react()
+
+    def disable(self, screen: Screen):
+        """Disables a Screen, if currently being shown, gets hidden"""
+        self.verify_tracked(screen)
+        if screen not in self.enabled_screens:
+            return  # has no effect
+
+        self.enabled_screens.remove(screen)
+        self._react()
+
+    def is_enabled(self, screen: Screen):
+        """Is the specified screen enabled?"""
+        return screen in self.enabled_screens
+
+    def get_set_to_show(self):
+        """What screens should get shown according to the current state"""
+        try:
+            priority_item = max(self.enabled_screens, key=lambda i: i.priority)
+            max_priority = priority_item.priority
+        except ValueError:
+            max_priority = -1 * math.inf
+        return {s for s in self.enabled_screens if s.priority == max_priority}
+
+    def _react(self):
+        """Reacts to the changes in Screen settings.
+        Sets active screens according to the Screen settings/state"""
+        if (new_set := self.get_set_to_show()) != self.active_set or \
+                any((s.changed for s in self.active_set)):
+            self.set_rewind()
+
+            self.active_set = new_set
+            for screen in self.active_set:
+                screen.changed = False
+            self.active_screens = sorted(new_set,
+                                         key=lambda i: i.order)
---
+++ tests/test_carousel.py
@@ -0,0 +1,147 @@
+"""Tests of the LCD Printer component"""
+from time import time
+
+from prusa.link.printer_adapter.structures.carousel import (  # type:ignore
+    Screen, LCDLine, Carousel)
+
+
+def test_line():
+    """Test that the LCDLine object works as expected"""
+    line = LCDLine("Derpy is best pony",
+                   delay=5,
+                   resets_idle=True,
+                   chime_gcode=["M300 S900 P1"])
+    assert line.text == "Derpy is best pony"
+    assert line.delay == 5
+    assert line.resets_idle
+    assert line.ends_at > time() + 4.5
+    assert line.chime_gcode == ["M300 S900 P1"]
+    line.ends_at = time() + 1
+    line.reset_end()
+    assert line.ends_at > time() + 4.5
+
+
+def test_screen_lines():
+    """Tests that a screen with set text outputs the correct lines
+    with the right properties attached"""
+    screen = Screen(resets_idle=False)
+    carousel = Carousel(screens=[screen])
+    carousel.set_text(
+        screen=screen,
+        text="I tried searching for an interesting text and got sidetracked.",
+        scroll_delay=1,
+        first_line_extra=3,
+        last_line_extra=5,
+        scroll_amount=3
+    )
+    lines = list(screen.lines())
+    first = lines[0]
+    second = lines[1]
+    second_to_last = lines[-2]
+    last = lines[-1]
+    assert first.text == "I tried searching f"
+    assert first.delay == 4
+    assert second.text == "ried searching for "
+    assert second.delay == 1
+    assert second_to_last.text == "and got sidetracked"
+    assert second_to_last.delay == 1
+    assert last.text == "nd got sidetracked."
+    assert last.delay == 6
+    assert len(lines) == 16
+
+    carousel.set_text(
+        screen=screen,
+        text="A",
+        scroll_delay=1,
+        first_line_extra=0,
+        last_line_extra=0
+    )
+    line = list(screen.lines())[0]
+    assert line.text == "A"
+    assert line.delay == 1
+
+
+def test_priority():
+    """Tests that the bigger priority gets shown and smaller gets hidden"""
+    screen_a = Screen(order=1)
+    screen_b = Screen(order=2)
+    screen_c = Screen(order=3)
+
+    carousel = Carousel(screens=[screen_a, screen_b, screen_c])
+    carousel.set_text(screen_a, "A")
+    carousel.set_text(screen_b, "B")
+    carousel.set_text(screen_c, "C")
+    carousel.set_priority(screen_a, 1)
+    carousel.set_priority(screen_b, 2)
+    carousel.set_priority(screen_c, 2)
+
+    carousel.enable(screen_b)
+    assert carousel.active_set == {screen_b}
+    carousel.enable(screen_a)
+    assert carousel.active_set == {screen_b}
+    carousel.enable(screen_c)
+    assert carousel.active_set == {screen_b, screen_c}
+    carousel.set_priority(screen_a, 3)
+    assert carousel.active_set == {screen_a}
+    carousel.disable(screen_a)
+    assert carousel.active_set == {screen_b, screen_c}
+    assert carousel.active_screens == [screen_b, screen_c]
+    carousel.set_priority(screen_a, 2)
+    carousel.enable(screen_a)
+    assert carousel.active_set == {screen_a, screen_b, screen_c}
+    assert carousel.active_screens == [screen_a, screen_b, screen_c]
+
+
+def test_lines():
+    """Tests the get_next output"""
+    screen_a = Screen(order=1, resets_idle=False)
+    screen_b = Screen(order=2, chime_gcode=["M300 S900 P1"], resets_idle=True)
+    screen_c = Screen(order=3)
+    carousel = Carousel(screens=[screen_a, screen_b, screen_c])
+    carousel.set_text(screen_a, "A")
+    carousel.set_text(screen_b, "B")
+    carousel.set_text(screen_c, "C")
+    carousel.set_priority(screen_a, 2)
+    carousel.set_priority(screen_b, 2)
+    carousel.set_priority(screen_c, 1)
+    carousel.enable(screen_a)
+    carousel.enable(screen_b)
+    carousel.enable(screen_c)
+
+    # Test normal operation, shows lines in order
+    a_line = carousel.get_next()
+    assert a_line.text == "A"
+    assert not a_line.resets_idle
+    b_line = carousel.get_next()
+    assert b_line.text == "B"
+    assert b_line.chime_gcode == ["M300 S900 P1"]  # and chimes
+    assert b_line.resets_idle
+    # This time around it shouldn't chime
+    assert carousel.get_next().text == "A"
+    # Second time around, it should not chime
+    assert carousel.get_next().chime_gcode == []
+    # messages have priority
+    carousel.add_message("asdf")
+    assert carousel.get_next().text == "asdf"
+    # Setting a hidden Screen does not rewind
+    assert carousel.get_next().text == "A"
+    carousel.set_text(screen_c, "Not C")
+    assert carousel.get_next().text == "B"
+    assert carousel.get_next().text == "A"
+    # setting a shown screen rewinds
+    carousel.set_text(screen_b, "Very much B")
+    assert carousel.get_next().text == "A"
+    assert carousel.get_next().text == "Very much B"
+    carousel.disable(screen_a)
+    # Enabling again does not reset the chime
+    carousel.enable(screen_b)
+    assert carousel.get_next().chime_gcode == []
+    carousel.disable(screen_b)
+    carousel.enable(screen_b)
+    assert carousel.get_next().chime_gcode == ["M300 S900 P1"]
+    carousel.disable(screen_b)
+    carousel.enable(screen_c)
+    assert carousel.get_next().text == "Not C"
+    carousel.disable(screen_a)
+    carousel.disable(screen_c)
+    assert carousel.get_next() is None
