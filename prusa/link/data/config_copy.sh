2e3ea0eb8d2b2012a91ac34d0d9d862c994ec967
Tomáš Jozífek <tomas.jozifek@prusa3d.cz>
2022-06-22 16:55:55 +0200

    If the config folder doesn't exist, create it.

--- prusa/link/data/config_copy.sh
+++ prusa/link/data/config_copy.sh
@@ -11,8 +11,13 @@ fi
 
 S_SOURCE="/boot/prusalink.ini"
 S_DESTINATION="/etc/prusalink/prusalink.ini"
+S_DESTINATION_DIR="/etc/prusalink"
 if test -f $S_SOURCE; then
     echo "Using the new app config from the boot partition!" | logger
+    if ! test -d S_DESTINATION_DIR ; then
+        echo "Creating a folder at $S_DESTINATION_DIR" | logger
+        mkdir $S_DESTINATION_DIR
+    fi
     mv $S_SOURCE $S_DESTINATION
 else
     echo "No file to overwrite the current app config with." | logger
