da5f9fd5ea92d74f6a34b7db2fd224cb3c81a467
Tomáš Jozífek <tomas.jozifek@prusa3d.cz>
2022-10-25 19:56:04 +0200

    Stop using udevadm, debian has too old of a version

--- prusa/link/camera.py
+++ prusa/link/camera.py
@@ -1,11 +1,13 @@
 """A place for Prusa-Link camera drivers"""
+import ctypes
+import fcntl
 import logging
-import subprocess
+from glob import glob
 from time import time, sleep
 
 import v4l2py  # type: ignore
 import v4l2py.raw  # type: ignore
-from v4l2py.device import PixelFormat  # type: ignore
+from v4l2py.device import PixelFormat, Device  # type: ignore
 
 from prusa.connect.printer.camera_driver import CameraDriver
 from prusa.connect.printer.camera import Resolution
@@ -13,6 +15,49 @@ from prusa.connect.printer.const import CapabilityType
 from .const import CAMERA_INIT_DELAY
 
 log = logging.getLogger(__name__)
+
+
+class MediaDeviceInfo(ctypes.Structure):
+    """A data structure for getting media device info"""
+    _fields_ = [
+        ("driver", ctypes.c_char * 16),
+        ("model", ctypes.c_char * 32),
+        ("serial", ctypes.c_char * 40),
+        ("bus_info", ctypes.c_char * 32),
+        ("media_version", ctypes.c_uint32),
+        ("hw_revision", ctypes.c_uint32),
+        ("driver_version", ctypes.c_uint32),
+        ("reserved", ctypes.c_uint32 * 31),
+    ]
+
+
+# pylint: disable=protected-access
+MEDIA_IOC_DEVICE_INFO = v4l2py.raw._IOWR('|', 0x00, MediaDeviceInfo)
+
+
+def read_media_device_info(path):
+    """Given a media device path, reads its associated info"""
+    info = MediaDeviceInfo()
+    # pylint: disable=unspecified-encoding
+    with open(path, "r") as file:
+        file_descriptor = file.fileno()
+        if fcntl.ioctl(file_descriptor, MEDIA_IOC_DEVICE_INFO, info):
+            raise RuntimeError("Failed getting media device info "
+                               f"for device {path}")
+    return info
+
+
+def get_media_device_path(device: Device):
+    """Gets the media device path for a video device
+
+    Pairs /dev/video* to /dev/media*"""
+    bus_info = device.info.bus_info
+    paths = glob("/dev/media*")
+    for path in paths:
+        info = read_media_device_info(path)
+        if bus_info == info.bus_info.decode("UTF-8"):
+            return path
+    return None
 
 
 def param_change(func):
@@ -43,21 +88,27 @@ class V4L2Driver(CameraDriver):
         available = {}
         devices = v4l2py.device.iter_video_capture_devices()
         for device in devices:
+            # Disqualify picameras - they need special treatment
+            if device.info.bus_info == "platform:bcm2835-isp":
+                continue
+
+            media_device_path = get_media_device_path(device)
+            if media_device_path is None:
+                continue
+
             path = str(device.filename)
             name = device.info.card
             try:
-                subprocess_result = subprocess.run(
-                    f"/usr/bin/udevadm info --name={path} --query=property "
-                    "--property=ID_SERIAL --value".split(" "),
-                    stdout=subprocess.PIPE, check=True)
-            except subprocess.CalledProcessError:
-                log.warning("Failed getting an id for camera on path: %s",
-                            device.filename)
-                # FIXME: Temporary picamera thingy
-                available["picamera"] = dict(path=path, name=name)
+                info = read_media_device_info(media_device_path)
+                serial = info.serial.decode("ascii")
+            except OSError:
+                log.exception("Getting camera sn failed for camera %s at %s",
+                              name, path)
                 continue
-            camera_id = subprocess_result.stdout.decode("ascii").strip()
-            available[camera_id] = dict(path=path, name=name)
+            else:
+                camera_id = " ".join((name, serial))
+                log.info("Camera id is %s", camera_id)
+                available[camera_id] = dict(path=path, name=name)
         return available
 
     def __init__(self, camera_id, config, unavailable_cb):
