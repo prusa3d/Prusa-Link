3c9d7fa0feb520e01a4868b58ab881d7f07ef42c
Ondrej Tuma <mcbig@zeropage.cz>
2022-11-21 10:06:34 +0100

    Static web update

--- config.custom.js
+++ config.custom.js
@@ -2,12 +2,12 @@ const webpackConfig = require("./webpack.config");
 
 module.exports = (env, args) => {
     const config = {
-        PRINTER_NAME: "Original Prusa MK3",
+        PRINTER_NAME: "Original Prusa i3",
         PRINTER_TYPE: "fdm",
 
         WITH_API_KEY_SETTING: true,
         WITH_SETTINGS: true,
-        WITH_CAMERA: true,
+        WITH_CAMERAS: true,
         WITH_CONTROLS: true,
         WITH_LOGS: true,
         WITH_V1_API: true,
