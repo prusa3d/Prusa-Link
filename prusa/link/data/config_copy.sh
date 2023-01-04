4c71fc103b0d84960fdc72510680f2ad3f3e1480
Tomáš Jozífek <tomas.jozifek@prusa3d.cz>
2023-01-04 15:05:03 +0100

    Make the config copy script user independent

--- prusa/link/data/config_copy.sh
+++ prusa/link/data/config_copy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+USERNAME=$(id -nu 1000)
+HOME_DIR=$(eval echo "~$USERNAME")
 P_SOURCE="/boot/prusa_printer_settings.ini"
-P_DESTINATION="/home/pi/prusa_printer_settings.ini"
+P_DESTINATION="$HOME_DIR/prusa_printer_settings.ini"
 if test -f $P_SOURCE; then
     echo "Using the new printer settings from the boot partition!" | logger
     mv $P_SOURCE $P_DESTINATION
-    chown pi $P_DESTINATION
+    chown $USERNAME $P_DESTINATION
+    chgrp $USERNAME $P_DESTINATION
+    chmod 644 $P_DESTINATION
 else
     echo "No file to overwrite the current printer settings with." | logger
 fi
@@ -19,6 +23,7 @@ if test -f $S_SOURCE; then
         mkdir $S_DESTINATION_DIR
     fi
     mv $S_SOURCE $S_DESTINATION
+    chmod 644 $S_DESTINATION
 else
     echo "No file to overwrite the current app config with." | logger
 fi
