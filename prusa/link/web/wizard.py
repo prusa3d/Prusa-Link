5562e862d60f18210966d860371c45010bbbac25
Michal Zoubek <michal.zoubek@prusa3d.cz>
2023-02-28 13:32:00 +0100

    Fixed wizard redirect when printer name is not inserted

--- prusa/link/web/wizard.py
+++ prusa/link/web/wizard.py
@@ -320,10 +320,12 @@ def wizard_finish_skip_post(req):
     # pylint: disable=unused-argument
     wizard = app.wizard
     printer = wizard.daemon.prusa_link.printer
-    connect_url = Printer.connect_url(wizard.connect_hostname,
-                                      bool(wizard.connect_tls),
-                                      wizard.connect_port)
-    printer.set_connection(connect_url, wizard.connect_token)
+
+    if wizard.restored_connect:
+        connect_url = Printer.connect_url(wizard.connect_hostname,
+                                          bool(wizard.connect_tls),
+                                          wizard.connect_port)
+        printer.set_connection(connect_url, wizard.connect_token)
 
     wizard.write_settings(app.settings)
 
