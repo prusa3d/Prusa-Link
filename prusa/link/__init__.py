a5fb8c2bbc15f45b6e5e2252dfe421dab19cc056
Ondrej Tuma <mcbig@zeropage.cz>
2023-05-23 09:39:47 +0200

    start new dev release

--- prusa/link/__init__.py
+++ prusa/link/__init__.py
@@ -5,8 +5,8 @@
 __application__ = "PrusaLink"
 __vendor__ = "Prusa Research"
 
-__version__ = "0.7.0"
-__date__ = "19 May 2023"
+__version__ = "0.7.1dev0"
+__date__ = "23 May 2023"
 __copyright__ = "(c) 2023 Prusa 3D"
 __author_name__ = "PrusaLink Developers"
 __author_email__ = "link@prusa3d.cz"
