5ba2a7468f7d9ec6c2048c35855f755153216ed4
Tomáš Jozífek <jozifektomas@gmail.com>
2023-12-07 21:07:06 +0100

    Fix the multi instance proxy for real this time

--- prusa/link/__init__.py
+++ prusa/link/__init__.py
@@ -5,8 +5,8 @@
 __application__ = "PrusaLink"
 __vendor__ = "Prusa Research"
 
-__version__ = "0.8.0alpha1"
-__date__ = "6 Dec 2023"
+__version__ = "0.8.0alpha2"
+__date__ = "11 Dec 2023"
 __copyright__ = "(c) 2023 Prusa 3D"
 __author_name__ = "PrusaLink Developers"
 __author_email__ = "link@prusa3d.cz"
